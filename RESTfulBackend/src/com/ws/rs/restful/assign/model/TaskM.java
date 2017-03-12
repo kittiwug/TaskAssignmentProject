c23bac70cd8e56be71954a1758d4ff22330a9ba3
Kitti Wugprakhon <anonwug@hotmail.com>
2017-03-12 12:43:21 +0700

    manage code.

--- RESTfulBackend/src/com/ws/rs/restful/assign/model/TaskM.java
+++ RESTfulBackend/src/com/ws/rs/restful/assign/model/TaskM.java
@@ -2,13 +2,10 @@ package com.ws.rs.restful.assign.model;
 
 import java.io.Serializable;
 
-import javax.xml.bind.annotation.XmlRootElement;
-
-@XmlRootElement(name = "taskm")
 public class TaskM implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private String idTask;
 	private String subjectTask;
 	private String subjectDetail;
@@ -21,20 +18,14 @@ public class TaskM implements Serializable {
 	public TaskM() {
 		// TODO Auto-generated constructor stub
 	}
-	
-	
-	
+
 	public String getIdTask() {
 		return idTask;
 	}
 
-
-
 	public void setIdTask(String idTask) {
 		this.idTask = idTask;
 	}
-
-
 
 	public String getSubjectTask() {
 		return subjectTask;
@@ -59,6 +50,7 @@ public class TaskM implements Serializable {
 	public void setStatus(String status) {
 		this.status = status;
 	}
+
 	public String getDateCreate() {
 		return dateCreate;
 	}
@@ -66,6 +58,7 @@ public class TaskM implements Serializable {
 	public void setDateCreate(String dateCreate) {
 		this.dateCreate = dateCreate;
 	}
+
 	public String getDateUpdate() {
 		return dateUpdate;
 	}
@@ -89,7 +82,5 @@ public class TaskM implements Serializable {
 	public void setEndDate(String endDate) {
 		this.endDate = endDate;
 	}
-	
-	
-	
+
 }
